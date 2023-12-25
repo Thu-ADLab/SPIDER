fix the bugs of collision check cuz of rotation

elements/curves.py
@@ -571,15 +571,15 @@ class InterpolationCurve(ExplicitCurve):
         return val
 
 
-
-class CubicSpline(InterpolationCurve):
+class spCubicSpline(InterpolationCurve):
     """
     CubicSpline class
     三次样条插值
+    todo: scipy自带的在距离很远的时候插值有严重问题，暂时先用自己写的，以后看看为什么有这个问题并且怎么修复
     """
     def __init__(self, x=None, y=None):
         self._sp_csp = None # 屈服了，还是用Scipy的吧
-        super(CubicSpline, self).__init__(x, y)
+        super(spCubicSpline, self).__init__(x, y)
 
     def _calc_coef(self):
         self._sp_csp = scipy.interpolate.CubicSpline(self.x, self.y)
@@ -745,6 +745,10 @@ class myCubicSpline(InterpolationCurve):
             pass  # because the higher order derivative is 0 anyway.
 
         return val
+
+
+class CubicSpline(myCubicSpline):
+    pass
 
 class Spline:#Cubic CubicSpline class(abandoned)
     def __init__(self, x, y):

elements/vector.py
@@ -37,7 +37,7 @@ def project(vector, segment_vector, calc_distance:bool=False):
 
 
 def rotate90(vec:np.ndarray):
-    return np.flip(vec)*[-1,0]
+    return np.flip(vec)*[-1,1]  # 离谱bug，1写成0了
 
 def rotate(array,anchor, angle, clockwise=False):
     '''

utils/collision/SAT.py
@@ -25,7 +25,7 @@ def SAT_check(vertices1:np.ndarray ,vertices2:np.ndarray):
                 v2 = polygon[0]
             else:
                 v2 = polygon[i+1]
-            vertical_vec = rotate90(v2-v1)
+            vertical_vec = rotate90(v2-v1) # v2-v1是1-2的边对应的向量
             separating_axis_vec.append(vertical_vec)
 
     # TODO: qzl:投影的时候其实也不用单位化法向量