ac656cdb52454d4457a63fbdb79e234ccf09068d
ZelinQian <qianzelin0@gmail.com>
2024-01-04 20:59:51 +0800

    pre-define some drawing functions

---
+++ visualize/elements.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+def draw_trackingbox_list():
+    pass
+
+def draw_trajectory():
+    pass
+
+def draw_trajectory_3d():
+    pass
+
+def draw_corridor_boxes_3d():
+    pass
+
+
+
+def show_occupancy():
+    pass
+
+
+
